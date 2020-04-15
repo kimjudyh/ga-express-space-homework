96d37bc7c75e0889b7e7d802bc9731955db5d0c7
Judy <kimjudyh@gmail.com>
2020-04-14 21:28:04 -0700

    added image urls for each mission, displayed on show view

--- models/marsMissions.js
+++ models/marsMissions.js
@@ -6,35 +6,35 @@ const marsMissions = [
     launchDate: "26 Nov 2011",
     operator: "NASA",
     missionType: "Rover",
-    img: ""
+    img: "https://mars.nasa.gov/system/resources/detail_files/24670_PIA23378-annotated-FINAL_1400.jpg"
   },
   {
     name: "Opportunity",
     launchDate: "8 Jul 2003",
     operator: "NASA",
     missionType: "Rover",
-    img: ""
+    img: "https://www.jpl.nasa.gov/missions/web/mer.jpg"
   },
   {
     name: "Spirit",
     launchDate: "10 Jun 2003",
     operator: "NASA",
     missionType: "Rover",
-    img: ""
+    img: "https://www.jpl.nasa.gov/missions/web/mer.jpg"
   },
   {
     name: "Sojourner",
     launchDate: "4 Dec 1996",
     operator: "NASA",
     missionType: "Rover",
-    img: ""
+    img: "https://www.nasa.gov/sites/default/files/styles/full_width_feature/public/thumbnails/image/pia01551.jpg"
   },
   {
     name: "Rosetta",
     launchDate: "2 Mar 2004",
     operator: "ESA",
     missionType: "Gravity Assist",
-    img: ""
+    img: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/04/Rosetta_and_Philae_%28crop%29.jpg/1280px-Rosetta_and_Philae_%28crop%29.jpg"
   }
 ];
 
